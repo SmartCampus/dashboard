d6653ddaf8b6fea150d56e08a38f5799a0522764
Etienne Strobbe <etienne.strobbe@gmail.com>
2014-07-16 23:19:35 +0200

    #6 #8 ajout d'une image selon tout type de forme vectorielle (path,rect,polygon)

--- admin/js/handling-svg.js
+++ admin/js/handling-svg.js
@@ -1,7 +1,7 @@
 $(document).ready(function($){
     
     $("plan-security").ready(function(){
-        load_svg("../data/plan_T1_4e.svg","plan-security",add_icon);
+        load_svg("./data/plan_T1_4e.svg","plan-security",add_icon);
     });
     
     var svg;
@@ -45,15 +45,46 @@ $(document).ready(function($){
      * correspondant à l'id en paramètre
      */
     function add_icon(id_salle){
-        var salle = svg.select('#'+id_salle).select('g').select("rect");
-        var x = salle.attr('x');
-        var y = salle.attr('y');
-        svg.append("image")
-            .attr("xlink:href","../data/img/test.jpg")
-            .attr('width', 20)
-            .attr('height', 24)
-            .attr('x', x)
-            .attr('y', y);
+        $.getJSON( "./data/alertes.json", function( data ) {
+            var alertes = data.alertes;
+            for(i=0;i<alertes.length;i++){
+                var kind = alertes[i].kind;
+                var x;
+                var y;
+                var id_to_find = alertes[i].id;
+                var salle = $("#"+id_to_find+">g").children().eq(0);
+                var balise = salle.get(0).nodeName;
+                if(balise == "rect"){
+                    var size_x = salle.attr('width')/3;
+                    var size_y = salle.attr('height')*3/4;                   
+                    x = parseFloat(salle.attr('x'));
+                    y = parseFloat(salle.attr('y'));
+                }
+                else if(balise == "path"){
+                    var points = (salle.attr("d")).split("-");
+                    var coord = points[0].split(",");
+                    // pour les portes
+                    x = coord[0].substring(1);
+                    y = coord[1].substring(0,coord[1].length-2);
+                }
+                else if(balise == "polygon"){
+                    var points = (salle.attr("points")).split(" ");
+                    var coord = points[0].split(",");
+                    // pour les portes
+                    x = coord[0];
+                    y = coord[1];
+                }
+                
+                // TODO (3)virer le polygon et gerer position des fenetres + (4)gerer mouseover + (1)legende + (2)list alerte ..... mouais
+                
+                 svg.append("image")
+                        .attr("xlink:href","./data/img/"+kind+".png")
+                        .attr('width', 20)
+                        .attr('height', 24)
+                        .attr('x', x)
+                        .attr('y', y);
+            }
+        });
     }
     
     
