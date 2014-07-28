4d9931c497f33f13bb4a6e9605f5318a2cc48ee4
Etienne Strobbe <etienne.strobbe@gmail.com>
2014-07-28 13:58:59 +0200

    #6 #24 affichage des parkings et de leur taux de remplissage

--- user/js/script.js
+++ user/js/script.js
@@ -1,14 +1,22 @@
 $(document).ready(function($){
+    var select = $("#select-day");
+    $(select).change(function(){
+        var day = $(this).val();
+        update_avg("data/moyenne-attente.json","moyenne-attente",day,"Temps d'attente","attente (minutes)","Temps moyen d'attente au RU");
+    });
+});
+
+function display_clock(id,title,url){
     /**
      * Partie concernant l'horloge
      * pour représenter le temps d'attente
      */
-    $.getJSON( "./data/temps.json", function( data ){
+    $.getJSON(url, function( data ){
         $("#title-time-left").append("Attente actuelle : <p id='number-time-left'>"+data.value+" mins</p>");
-       var chart = AmCharts.makeChart("time-left", {
+       var chart = AmCharts.makeChart(id, {
            "titles": [
                 {
-                    "text": "Heure à laquelle vous mangerez : ",
+                    "text": title,
                     "size": 13
                 }
             ],
@@ -55,7 +63,7 @@ $(document).ready(function($){
             }
         });
         // update each second
-        setInterval(updateClock, 1000);
+        setTimeout(updateClock, 1000);
 
 
         // update clock
@@ -72,82 +80,141 @@ $(document).ready(function($){
             chart.arrows[1].setValue((12 * ((minutes+data.value) + seconds / 60) / 60));
             // set seconds
             chart.arrows[2].setValue(12 * date.getSeconds() / 60);
+            clearTimeout();
         }
     });
+}
     
-    
-    /* ###################################################################
-    ########                                                       #######
-    ########            Widget moyennes (barres)                   #######
-    ########                                                       #######
-    ######################################################################*/
-    var select = $("#select-day");
-    $(select).change(function(){
-        var day = $(this).val();
-        update_avg(day);
+  
+/* ###################################################################
+########                                                       #######
+########            Widget moyennes (barres)                   #######
+########                                                       #######
+######################################################################*/
+
+function update_avg(url,id_div,day,x_legende,y_legende,title){
+    $.getJSON(url, function(data) {
+        var avgs = [];
+        var dates = [];
+        var id = data.id;
+        var values;
+        switch(parseInt(day)){
+                case 1:values = data.day_1;break;
+                case 2:values = data.day_2;break;
+                case 3:values = data.day_3;break;
+                case 4:values = data.day_4;break;
+                case 5:values = data.day_5;break;
+                default:values = data.day_1;break;
+        }
+        for(i=0;i<values.length;i++){
+            avgs[i] = values[i].value;
+            dates[i] = values[i].date;
+        }
+        set_values_charts(id_div,dates,avgs,x_legende,y_legende,title);
+        
     });
-    function update_avg(day){
-        $.getJSON('./data/moyenne-attente.json', function(data) {
-            var avgs = [];
-            var dates = [];
-            var id = data.id;
-            var values;
-            switch(parseInt(day)){
-                    case 1:values = data.day_1;break;
-                    case 2:values = data.day_2;break;
-                    case 3:values = data.day_3;break;
-                    case 4:values = data.day_4;break;
-                    case 5:values = data.day_5;break;
-                    default:values = data.day_1;break;
+}
+
+function set_values_charts(id,dates,avgs,x_legende,y_legende,title){
+    $("#"+id).highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: title
+        },
+        subtitle: {
+            text: 'Source: SmartCampus'
+        },
+        xAxis: {
+            categories: dates
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: y_legende
             }
-            for(i=0;i<values.length;i++){
-                avgs[i] = values[i].value;
-                dates[i] = values[i].date;
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} min</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
             }
+        },
+        series: [{
+            name: x_legende,
+            data: avgs
+
+        }]
+    });
+}
 
 
-            $('#moyenne-attente').highcharts({
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Temps moyen d\'attente au RU'
-                },
-                subtitle: {
-                    text: 'Source: SmartCampus'
-                },
-                xAxis: {
-                    categories: dates
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'attente (minutes)'
-                    }
-                },
-                tooltip: {
-                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                        '<td style="padding:0"><b>{point.y:.1f} min</b></td></tr>',
-                    footerFormat: '</table>',
-                    shared: true,
-                    useHTML: true
-                },
-                plotOptions: {
-                    column: {
-                        pointPadding: 0.2,
-                        borderWidth: 0
-                    }
-                },
-                series: [{
-                    name: 'Temps d\'attente',
-                    data: avgs
 
-                }]
-            });
-        });
+function init_gauge_park(id,value,parking){
+    var gaugeChart = AmCharts.makeChart(id, {
+        "titles": [
+            {
+                "text": "Parking "+parking,
+                "size": 13
+            }
+        ],
+        "type": "gauge",
+        "theme": "none",    
+        "axes": [{
+            "axisThickness":1,
+             "axisAlpha":0.2,
+             "tickAlpha":0.2,
+             "valueInterval":20,
+            "bands": [{
+                "color": "#84b761",
+                "endValue": 45,
+                "innerRadius": "0%",
+                "startValue": 0
+            }, {
+                "color": "#fdd400",
+                "endValue": 75,
+                "innerRadius": "0%",
+                "startValue": 45
+            }, {
+                "color": "#cc4748",
+                "endValue": 95,
+                "innerRadius": "0%",
+                "startValue": 75
+            },{
+                "color": "black",
+                "endValue": 100,
+                "innerRadius": "0%",
+                "startValue": 95
+            }],
+            "bottomText": "0 %",
+            "bottomTextYOffset": -20,
+            "endValue": 100
+        }],
+        "arrows": [{
+            "color":"#046380",
+            "nailRadius": 4,
+            "startWidth": 6,
+            "innerRadius": 0,
+            "borderAlpha":1
+        }]
+    });
+    setTimeout(setValue, 1000);
+
+     // set random value
+    function setValue() {
+        gaugeChart.arrows[0].setValue(value);
+        gaugeChart.axes[0].setBottomText(value + " %");
+        clearTimeout();
     }
-    update_avg(0);
+}
     
     
-});
