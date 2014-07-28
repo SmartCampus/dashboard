f9ea79006c19885e76dacf28ec77a5da5287efbe
Etienne Strobbe <etienne.strobbe@gmail.com>
2014-07-28 11:14:49 +0200

    #6 update lib (data en paramètre et non url d'un json) + doc lib

--- admin_select/js/plan.js
+++ admin_select/js/plan.js
@@ -1,6 +1,5 @@
 $(document).ready(function($){
-    var legend = { temp:"Température en °C",
-                  other:"Présence capteur = 1"}
+    
     $('#form_radio').hide();
     var heatmap;
     
@@ -21,7 +20,7 @@ $(document).ready(function($){
             var already_checked = $(this).prop('checked');
             if(checked){
                 this.checked = true;
-                if(!already_checked)put_sensors(kind,"data/sensors.json");
+                if(!already_checked)load_and_launch("data/sensors.json",put_sensors,kind);
             }
             else{
                 this.checked = false;
@@ -81,10 +80,10 @@ $(document).ready(function($){
         var kind = $(this).attr('id').split('_')[1];
         uncheck_all_box($(this));
         if($(this).prop("checked")){
-            put_sensors(kind,"data/sensors.json");
+            load_and_launch("data/sensors.json",put_sensors,kind);
         }
         else{
-            unput_sensors(kind,"data/sensors.json");
+            load_and_launch("data/sensors.json",unput_sensors,kind);
         }
         
     });
@@ -93,80 +92,11 @@ $(document).ready(function($){
         var kind = $(this).attr('id').split('_')[1];
         if($(this).prop("checked")){
             remove_canvas();
-            init_heatmap(kind);
-            // TODO revoir fonction init avec kind + desactiver capteur sur switch
+            load_data_heatmap("data/sensors.json",kind);
         }
     });
     
-    
-    function init_heatmap(kind_wanted,heatmap){
-        $.getJSON('data/sensors.json',function(data){
-            var sensors = data.sensors;
-            var data = [];
-            var max = 45;
-            var title = legend["other"];
-            for(i=0;i<sensors.length;i++){
-                var kind = sensors[i].kind;
-                if(kind == kind_wanted){
-                    var salle_svg = $("#"+sensors[i].salle+">g").children().eq(0);
-
-                    var balise = salle_svg.get(0).nodeName;              
-                    var x_tmp,y_tmp,size_x,size_y;
-                    // on affecte les valeurs de x et y selon la forme vectorielle de la salle
-
-                    if(balise == "rect"){    
-                        size_x = parseFloat(salle_svg.attr('width'));
-                        size_y = parseFloat(salle_svg.attr('height'));
-                        x_tmp = parseFloat(salle_svg.attr('x'));
-                        y_tmp = parseFloat(salle_svg.attr('y'));
-                    }
-                    var value;
-                    var actual_value = sensors[i].value;
-                    if(kind_wanted != "temp"){
-                        if(actual_value){
-                            value = 1;
-                        }
-                        else{
-                            value = 0;
-                        }
-                        max = 1;
-                    }
-                    else{
-                        value = actual_value;
-                        title = legend["temp"];
-                    }
-                    console.log(value);
-                    data.push({
-                        x : x_tmp + size_x/2,
-                        y : y_tmp + size_y/2,
-                        count : value
-                    });
-                }            
-            }
-            // heatmap configuration
-            var config = {
-                element: document.getElementById("plan-select"),
-                radius: 35,
-                opacity: 50,
-                legend: {
-                    position: 'br',
-                    title: title
-                }
-            };
-
-            //creates and initializes the heatmap
-            heatmap = h337.create(config);
-
-            // let's get some data
-            var data = {
-                max: max,
-                data: data
-            };
-
-            heatmap.store.setDataSet(data);
-        });
-        
-    }    
+       
     function remove_canvas(){
         $('canvas').remove();
         $("#plan-select>div").remove();
