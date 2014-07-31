ef680415a99d7196862857aecd5e60b892b436f1
Etienne Strobbe <etienne.strobbe@gmail.com>
2014-07-31 13:29:08 +0200

    #6 #28 affichage du bâtiment le moins occupé sur la carte

---
+++ user/js/libre-map.js
@@ -0,0 +1,24 @@
+google.maps.event.addDomListener(window, 'load', initialize);
+insert_marker("temp1");
+
+function insert_marker(bat_wanted){
+    $.getJSON("data/coord_poi.json", function( data ){
+        var coords = data.coords;
+        for(i=0;i<coords.length;i++){
+            var bat = coords[i].bat;
+            if(bat == bat_wanted){
+                var latLng = new google.maps.LatLng(coords[i].lat,coords[i].lng);
+                var marker = new google.maps.Marker({
+                    position : latLng,
+                    map:map,
+                    name:bat
+                });
+                google.maps.event.addListener(marker,'click',function(event){
+                    tooltip.setContent(bat_wanted+" est le bâtiment le moins occupé");
+                    tooltip.open(map,this);
+                });
+                break;
+            }
+        }
+    });
+}
