9032a592453e8fbe5db716b07eb475826ff45041
Etienne Strobbe <etienne.strobbe@gmail.com>
2014-08-06 11:38:47 +0200

    #6 #43 nombre d'alerte par type (secu et energie) dans la navbar

--- admin/js/script.js
+++ admin/js/script.js
@@ -18,6 +18,22 @@ function display_list_warning(kind_warning){
                 size++;
             }
         }
-        $("#title-alerte").append(" <span class=\"badge\">"+size+"</span>");
+        $("#title-alerte").append(" <span class=\"badge badge-error\">"+size+"</span>");
     });
+}
+
+function display_alert_number(){
+    $.getJSON("data/alertes.json",function( data ){
+        var sensors = data.sensors;
+        var number_security = 0;
+        var number_energy = 0;
+        for(var i=0;i<sensors.length;i++){
+            var kind = sensors[i].kind;
+            if(kind == "light" || kind == "temp")number_energy++;
+            if(kind == "door" || kind == "window")number_security++;
+        }
+        $("#title-alerte-security").append(" <span class=\"badge badge-error\">"+number_security+"</span>");
+        $("#title-alerte-energy").append(" <span class=\"badge badge-error\">"+number_energy+"</span>");
+    });
+    
 }
